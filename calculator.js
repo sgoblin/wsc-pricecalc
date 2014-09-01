253e92adb060cc7d6ad1a276bf6dc1f7d5fc72b5
sgoblin <rsuper@sonic.net>
2014-09-01 00:37:59 -0400

    Added variables and a hour to minute function to js file, created style1.css, changed self closing tag to separate tag in html.

---
+++ calculator.js
@@ -0,0 +1,19 @@
+var 21rate = 50;
+var duoDiscusRate = 75;
+var 23Rate = 45;
+var 24Rate = 45;
+/*The rates for the sailplanes are ^ */
+
+var insWSCAC = 60;
+var insYouAC = 70;
+var insGrnd = 60;
+/*The rates for instruction, in format ins<Aircraft><Place> ex. ins<WSC aircraft><In aircraft> = insWSCAC */
+
+var tow100 = 1.4;
+/*The rate per hundred feet of tow.*/
+
+function hr2min(hourTime) {
+	return hourTime*60;
+}
+//Turns hours into minutes.
+
